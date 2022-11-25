512d64d980d37a81016115f3a89b60d5ab623b75
diegobsailva10 <diegobdasilva12@gmail.com>
2022-11-25 16:07:28 -0300

    Evento de Click

--- app/src/main/java/com/example/app_gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/app_gastodeviagem/MainActivity.kt
@@ -2,18 +2,27 @@ package com.example.app_gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.app_gastodeviagem.databinding.ActivityMainBinding
+import android.widget.Button
+import android.widget.*
 
-class MainActivity : AppCompatActivity() {
-
-    //Definida no escopo da classe
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =  ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        //Evento de click
+        val buttonCalcular: Button = findViewById(R.id.filled_tonal_button)
+        buttonCalcular.setOnClickListener{
+            Toast.makeText(this,"EVENTO DE CLICK FUNCIONANDO",Toast.LENGTH_SHORT).show()
+        }
+
 
     }
+
+    private fun calculate() {
+        TODO("Not yet implemented")
+    }
+
 
 }
